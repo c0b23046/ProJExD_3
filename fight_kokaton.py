1d0d2a933acb962ea3f7e0d2c968fe2dd4f6ea09
C0B23046 <c0b23046c1@edu.teu.ac.jp>
2024-06-25 17:22:55 +0900

    追加機能１；scoreひょうじ

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -148,6 +148,24 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Score:
+    """
+    スコア表示に関するクラス
+    """
+
+    def __init__(self):
+        self.fonto = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+        self.color = (0, 0, 255)
+        self.score = 0
+        self.img = self.fonto.render(f"スコア:", self.score, self.color)
+        self.rct = self.img.get_rect()
+        self.rct.center = 100, HEIGHT - 50
+
+    def update(self, screen: pg.Surface):
+        self.img = self.fonto.render(f"スコア:{self.score}", self.score, self.color)
+        screen.blit(self.img, self.rct)
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -156,6 +174,7 @@ def main():
     beam = None
     # bomb = Bomb((255, 0, 0), 10)
     bombs = [Bomb((255, 0, 0), 10) for _ in range(NUM_OF_BOMBS)]
+    score = Score()
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -164,7 +183,8 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 # スペースキー押下でBeamクラスのインスタンス生成
-                beam = Beam(bird)            
+                beam = Beam(bird)
+
         screen.blit(bg_img, [0, 0])
 
         for bomb in bombs:
@@ -177,13 +197,13 @@ def main():
                 pg.display.update()
                 time.sleep(5)
                 return
-            
+        
         for i in range(len(bombs)):
             if beam is not None:
                 if bombs[i].rct.colliderect(beam.rct):
                     bombs[i] = None 
-                    beam = None
                     bird.change_img(6,screen)
+                    score.score += 1
         bombs = [bomb for bomb in bombs if bomb is not None]
 
         key_lst = pg.key.get_pressed()
@@ -192,7 +212,7 @@ def main():
             beam.update(screen)
         for bomb in bombs:
             bomb.update(screen)
-        
+        score.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
