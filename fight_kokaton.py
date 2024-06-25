292ca055797a4bc1bcac243410173c82a295d8c4
C0B23046 <c0b23046c1@edu.teu.ac.jp>
2024-06-25 14:42:33 +0900

    れんしゅう３：よろこびのまい

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -165,11 +165,13 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
+            
         if bomb is not None:
             if beam is not None:
                 if bomb.rct.colliderect(beam.rct):
                     bomb = None 
                     beam = None
+                    bird.change_img(6,screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
